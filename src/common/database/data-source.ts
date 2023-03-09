64faf3788dabe85da30afe7b1537988879e0bf58
Prem <premdasvm8@gmail.com>
2023-03-09 15:49:42 +0530

    fix: typeorm options centralized for cli and orm  - SnakeNamingStrategy added.

--- src/common/database/data-source.ts
+++ src/common/database/data-source.ts
@@ -1,18 +1,21 @@
-import { DataSource, DataSourceOptions } from 'typeorm';
+import { DataSource } from 'typeorm';
 import { ConfigService } from '@nestjs/config';
 import { config } from 'dotenv';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import { IConfig } from '@lib/config/config.interface';
 
 config();
 
-const configService = new ConfigService();
+const configService = new ConfigService<IConfig, true>();
 
-export default new DataSource({
+export const connectionOptions = {
   type: 'postgres',
-  host: configService.get('DB_HOST'),
-  port: configService.get('DB_PORT'),
-  username: configService.get('DB_USERNAME'),
-  password: configService.get('DB_PASSWORD'),
-  database: configService.get('DB_DATABASE'),
+  host: configService.get('database.host', { infer: true }),
+  port: configService.get('database.port', { infer: true }),
+  username: configService.get('database.user', { infer: true }),
+  password: configService.get('database.password', { infer: true }),
+  database: configService.get('database.dbName', { infer: true }),
   synchronize: false,
   dropSchema: false,
   migrationsRun: false,
@@ -20,9 +23,12 @@ export default new DataSource({
   entities: ['dist/**/*.entity{.ts,.js}'],
   subscribers: ['dist/**/*.subscriber{.ts,.js}'],
   migrations: ['migrations/**/*{.ts,.js}'],
+  namingStrategy: new SnakeNamingStrategy(),
   cli: {
     entitiesDir: 'src',
     migrationsDir: 'migrations',
     subscribersDir: 'src',
   },
-} as DataSourceOptions);
+} as PostgresConnectionOptions;
+
+export default new DataSource(connectionOptions);
--- src/common/database/typeorm-config.service.ts
+++ src/common/database/typeorm-config.service.ts
@@ -1,32 +1,12 @@
-import { IConfig } from '@lib/config/config.interface';
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { connectionOptions } from './data-source';
 
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
-  constructor(private configService: ConfigService<IConfig, true>) {}
+  constructor() {}
 
   createTypeOrmOptions(): TypeOrmModuleOptions {
-    return {
-      type: 'postgres',
-      host: this.configService.get('database.host', { infer: true }),
-      port: this.configService.get('database.port', { infer: true }),
-      username: this.configService.get('database.user', { infer: true }),
-      password: this.configService.get('database.password', { infer: true }),
-      database: this.configService.get('database.dbName', { infer: true }),
-      autoLoadEntities: true,
-      synchronize: false,
-      dropSchema: false,
-      keepConnectionAlive: true,
-      logging: true,
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
-      cli: {
-        entitiesDir: 'src',
-        migrationsDir: 'migrations',
-        subscribersDir: 'subscriber',
-      },
-    } as TypeOrmModuleOptions;
+    return connectionOptions;
   }
 }
