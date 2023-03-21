3cbb45f7744c059f35ed711eebf9466162f646eb
Prem <premdasvm8@gmail.com>
2023-03-22 00:29:46 +0530

    feat(static serving): Static Serving module added.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 export * from "./config/config.module";
 export * from "./orm/orm.module";
 export * from "./jwt/jwt.module";
+export * from "./serve-static/serve-static.module";
--- src/modules/shared/shared.module.ts
+++ src/modules/shared/shared.module.ts
@@ -1,10 +1,11 @@
-import { NestConfigModule } from '@lib/index';
-import { OrmModule } from '@lib/orm/orm.module';
-import { AuthModule } from '@modules/auth/auth.module';
-import { UserModule } from '@modules/user/user.module';
-import { Module } from '@nestjs/common';
+import { NestConfigModule } from "@lib/index";
+import { OrmModule } from "@lib/orm/orm.module";
+import { AuthModule } from "@modules/auth/auth.module";
+import { UserModule } from "@modules/user/user.module";
+import { Module } from "@nestjs/common";
+import { ServeStaticModule } from "@nestjs/serve-static";
 
 @Module({
-  imports: [OrmModule, NestConfigModule, UserModule, AuthModule],
+	imports: [OrmModule, NestConfigModule, UserModule, AuthModule, ServeStaticModule],
 })
 export class SharedModule {}
