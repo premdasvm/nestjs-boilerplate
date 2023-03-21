ac325d9be3ac57448fbea5eb0ea1c36440952cc8
Prem <premdasvm8@gmail.com>
2023-03-22 00:35:39 +0530

    fix(static serving): Module import issue fixed.

--- src/modules/shared/shared.module.ts
+++ src/modules/shared/shared.module.ts
@@ -1,11 +1,10 @@
-import { NestConfigModule } from "@lib/index";
+import { NestConfigModule, NestServeStaticModule } from "@lib/index";
 import { OrmModule } from "@lib/orm/orm.module";
 import { AuthModule } from "@modules/auth/auth.module";
 import { UserModule } from "@modules/user/user.module";
 import { Module } from "@nestjs/common";
-import { ServeStaticModule } from "@nestjs/serve-static";
 
 @Module({
-	imports: [OrmModule, NestConfigModule, UserModule, AuthModule, ServeStaticModule],
+	imports: [OrmModule, NestConfigModule, UserModule, AuthModule, NestServeStaticModule],
 })
 export class SharedModule {}
